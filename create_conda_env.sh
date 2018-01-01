382e94da49a1ce09ebf8c9817a3ddb7fed82c167
J35P312 <jesper.eisfeldt@scilifelab.se>
2018-01-01 23:27:29 +0100

    new file:   create_conda_env.sh 	new file:   misc/distance_calculator.py

---
+++ create_conda_env.sh
@@ -0,0 +1,15 @@
+cd $1
+conda config --add channels r
+conda config --add channels bioconda
+
+conda create --force -n SplitVision python=2.7
+source activate SplitVision
+
+conda install -c bioconda cd-hit --yes
+conda install -c biobuilds clustalw --yes
+conda install -c bioconda abyss --yes
+conda install freebayes --yes
+conda install vt --yes
+pip install xlwt
+
+source deactivate
